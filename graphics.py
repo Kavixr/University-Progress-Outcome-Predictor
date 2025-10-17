26ad9e764fe83a33dcfc3e3b70ff43ee327255e8
Kavindu Rajapaksha <kavindurajapaksha848@gmail.com>
2025-10-18 00:26:47 +0530

    update the code

--- graphics.py
+++ graphics.py
@@ -756,3 +756,136 @@ class Entry(GraphicsObject):
         self.color=color
         if self.entry:
             self.entry.config(fg=color)
+
+
+class Image(GraphicsObject):
+
+    idCount = 0
+    imageCache = {} # tk photoimages go here to avoid GC while drawn 
+    
+    def __init__(self, p, *pixmap):
+        GraphicsObject.__init__(self, [])
+        self.anchor = p.clone()
+        self.imageId = Image.idCount
+        Image.idCount = Image.idCount + 1
+        if len(pixmap) == 1: # file name provided
+            self.img = tk.PhotoImage(file=pixmap[0], master=_root)
+        else: # width and height provided
+            width, height = pixmap
+            self.img = tk.PhotoImage(master=_root, width=width, height=height)
+                
+    def _draw(self, canvas, options):
+        p = self.anchor
+        x,y = canvas.toScreen(p.x,p.y)
+        self.imageCache[self.imageId] = self.img # save a reference  
+        return canvas.create_image(x,y,image=self.img)
+    
+    def _move(self, dx, dy):
+        self.anchor.move(dx,dy)
+        
+    def undraw(self):
+        try:
+            del self.imageCache[self.imageId]  # allow gc of tk photoimage
+        except KeyError:
+            pass
+        GraphicsObject.undraw(self)
+
+    def getAnchor(self):
+        return self.anchor.clone()
+        
+    def clone(self):
+        other = Image(Point(0,0), 0, 0)
+        other.img = self.img.copy()
+        other.anchor = self.anchor.clone()
+        other.config = self.config.copy()
+        return other
+
+    def getWidth(self):
+        """Returns the width of the image in pixels"""
+        return self.img.width() 
+
+    def getHeight(self):
+        """Returns the height of the image in pixels"""
+        return self.img.height()
+
+    def getPixel(self, x, y):
+        """Returns a list [r,g,b] with the RGB color values for pixel (x,y)
+        r,g,b are in range(256)
+
+        """
+        
+        value = self.img.get(x,y) 
+        if type(value) ==  type(0):
+            return [value, value, value]
+        else:
+            return list(map(int, value.split())) 
+
+    def setPixel(self, x, y, color):
+        """Sets pixel (x,y) to the given color
+        
+        """
+        self.img.put("{" + color +"}", (x, y))
+        
+
+    def save(self, filename):
+        """Saves the pixmap image to filename.
+        The format for the save image is determined from the filname extension.
+
+        """
+        
+        path, name = os.path.split(filename)
+        ext = name.split(".")[-1]
+        self.img.write( filename, format=ext)
+
+        
+def color_rgb(r,g,b):
+    """r,g,b are intensities of red, green, and blue in range(256)
+    Returns color specifier string for the resulting color"""
+    return "#%02x%02x%02x" % (r,g,b)
+
+def test():
+    win = GraphWin()
+    win.setCoords(0,0,10,10)
+    t = Text(Point(5,5), "Centered Text")
+    t.draw(win)
+    p = Polygon(Point(1,1), Point(5,3), Point(2,7))
+    p.draw(win)
+    e = Entry(Point(5,6), 10)
+    e.draw(win)
+    win.getMouse()
+    p.setFill("red")
+    p.setOutline("blue")
+    p.setWidth(2)
+    s = ""
+    for pt in p.getPoints():
+        s = s + "(%0.1f,%0.1f) " % (pt.getX(), pt.getY())
+    t.setText(e.getText())
+    e.setFill("green")
+    e.setText("Spam!")
+    e.move(2,0)
+    win.getMouse()
+    p.move(2,3)
+    s = ""
+    for pt in p.getPoints():
+        s = s + "(%0.1f,%0.1f) " % (pt.getX(), pt.getY())
+    t.setText(s)
+    win.getMouse()
+    p.undraw()
+    e.undraw()
+    t.setStyle("bold")
+    win.getMouse()
+    t.setStyle("normal")
+    win.getMouse()
+    t.setStyle("italic")
+    win.getMouse()
+    t.setStyle("bold italic")
+    win.getMouse()
+    t.setSize(14)
+    win.getMouse()
+    t.setFace("arial")
+    t.setSize(20)
+    win.getMouse()
+    win.close()
+
+if __name__ == "__main__":
+    test()
